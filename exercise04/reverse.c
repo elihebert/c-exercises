1940bdd0abfb9cb27ef838cf01dd7ac568f95495
Eli Hebert <hebea-25@rhodes.edu>
2023-03-07 14:13:06 -0600

    kata4 complete

--- exercise04/reverse.c
+++ exercise04/reverse.c
@@ -5,11 +5,24 @@
 // function begins by allocating array of pointers to strings, while keeping
 // track of the current size and the maximum capacity of the array (at that
 // point); enters loop that begins to read lines from standard input using fgets
-// while allocating memory for said lines using malloc, and adds it to the array
+// while allocating memory for said lines using malloc, and adds it to the
+// array; if the array is full, the realloc function is used to double its
+// capacity; after all input has been read and stored, the function allocates an
+// array to hold the intergers, and then uses strtok and atoi to split each line
+// into integers (tokens) and stores them in the array. The final part of the
+// function prints out the integers in the order which they were read from the
+// array/stored, which is reversed; assumes the input contains only valid
+// integers that are separated by spaces; no line of input can be over 99
+// characters
 
 void readAndPrint() {
   int cap = 1;
   int size = 0;
+  // allocates block of memory dynamically, with size of cap times the size of a
+  // pointer to a character determined by sizeof; result is founter to first
+  // byte of block of memory; by alocating cap times char* pointers, it uses
+  // lines to store an array of strings, with each pointer pointing to the first
+  // character of the string
   char **linesR = malloc(cap * sizeof(char *));
 
   while (1) {
@@ -52,20 +65,8 @@ void readAndPrint() {
   free(linesR);
 }
 
-//
-//
-//
-//
-//
-//
 int main() {
   readAndPrint();
 
   return 0;
 }
-
-//
-//
-//
-//
-//
